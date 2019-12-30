d1dcf3e3297ffc0fa202194864c7120307cced4c
rohitp1995 <rohit050695@gmail.com>
2019-12-30 19:03:09 +0530

    Add files via upload
    python code for the game

---
+++ BULLS AND COWS GAME.py
@@ -0,0 +1,38 @@
+import random
+
+rand=random.randint(1000,9999)
+
+random=[]
+
+for i in range(len(str(rand))):
+    random.append(str(rand)[i])
+
+Repeat='Yes'
+Guess=0
+
+while Repeat=='Yes':
+    print(random)
+    user_input=int(input("enter a number"))
+    user_input_list=[]
+    cows=0
+    bulls=0
+    for j in range(len(str(user_input))):
+        user_input_list.append(str(user_input)[j])
+        
+    for i in range(len(user_input_list)):
+        if user_input_list[i]==random[i]:
+            cows=cows+1
+        elif user_input_list[i] not in random[i] and user_input_list[i] in random:
+            bulls=bulls+1
+
+
+    print("bulls=",bulls,"   cows=",cows)
+    
+
+    if user_input_list==random:
+        Repeat='No'
+    else:
+        Repeat='Yes'
+    Guess=Guess+1
+else:
+    print("YOU GUESSED IT CORRECT in",Guess,"Guess")
